c7f539d7b5d0a4ea4cec7b531fb9fd41f0462213
Daryl Walleck <dwalleck@gmail.com>
2016-11-14 13:10:07 -0600

    Make all scripts executable during setup phase

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
+chmod +755 bootstrap-devstack.sh
+chmod +755 pre-start.sh
+chmod +755 prep-guest.sh
+chmod +755 run-devstack.sh
+chmod +755 teardown.sh
+
 lxc-create -t ubuntu -n devstack -f devstack.conf
 lxc-start --name devstack
