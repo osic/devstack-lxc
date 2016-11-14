2cde1637343dd1c02a5781a02e31256eb8cb4c2d
Daryl Walleck <dwalleck@gmail.com>
2016-11-14 13:15:03 -0600

    Make path absolute for prep-guest.sh

--- bootstrap-devstack.sh
+++ bootstrap-devstack.sh
@@ -1,5 +1,5 @@
 cp prep-guest.sh /var/lib/lxc/devstack/rootfs/root
-lxc-attach -n devstack -- bash -c "echo ./prep-guest.sh | sh"
+lxc-attach -n devstack -- bash -c "echo /root/prep-guest.sh | sh"
 cp localrc /var/lib/lxc/devstack/rootfs/opt/stack/devstack
 cp run-devstack.sh /var/lib/lxc/devstack/rootfs/opt/stack
 lxc-attach -n devstack -- bash -c "echo sudo -H -u stack /opt/stack/run-devstack.sh | sh"
