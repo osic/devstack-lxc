1fd012f4ad78647082214c7db9f4ce9b3354bcd3
Castulo J. Martinez <castulo.martinez@intel.com>
2016-12-15 14:53:07 -0800

    Use local.conf file instead of localrf
    This commit also include changes in the config file for devstack
    multi-node.

--- bootstrap-devstack.sh
+++ bootstrap-devstack.sh
@@ -1,5 +1,11 @@
+# Prepare the devstack source files
 cp prep-guest.sh /var/lib/lxc/devstack-node/rootfs/root
 lxc-attach -n devstack-node -- bash -c "echo /root/prep-guest.sh | sh"
-cp localrc /var/lib/lxc/devstack/rootfs/opt/stack/devstack
-cp run-devstack.sh /var/lib/lxc/devstack/rootfs/opt/stack
-lxc-attach -n devstack -- bash -c "echo sudo -H -u stack /opt/stack/run-devstack.sh | sh"
+
+# Copy the devstack configuration file to the container
+# TODO: Make changes in the IPs of the config file
+cp local.conf /var/lib/lxc/devstack-node/rootfs/opt/stack/devstack
+
+# Run the script to install devstack
+cp run-devstack.sh /var/lib/lxc/devstack-node/rootfs/opt/stack
+lxc-attach -n devstack-node -- bash -c "echo sudo -H -u stack /opt/stack/run-devstack.sh | sh"
