413c60223ba45351d094e8aab7dd109e7eaa981d
Castulo J. Martinez <castulo.martinez@intel.com>
2016-12-15 12:41:59 -0800

    Adding a message when the container fails to start

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ chmod +755 teardown.sh
 # Create and start the container
 echo "Creating the lxc container ..."
 lxc-create -t ubuntu -n devstack-node -f devstack-node.conf
-echo "Starting the lxc container for the first time ..."
-lxc-start --name devstack-node
-echo "The lxc container devstack-node has been created and is active."
+echo "Starting the lxc container  ..."
+lxc-start --name devstack-node && \
+echo "The lxc container devstack-node is active." || \
+echo "The lxc container devstack-node failed to be started."
