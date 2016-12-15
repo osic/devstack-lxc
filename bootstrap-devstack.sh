9db49c850106475c69f68e39e85f8bdc9c53b2a8
Castulo J. Martinez <castulo.martinez@intel.com>
2016-12-15 14:45:14 -0800

    Changing the script to use oficial devstack repo

--- bootstrap-devstack.sh
+++ bootstrap-devstack.sh
@@ -1,5 +1,5 @@
-cp prep-guest.sh /var/lib/lxc/devstack/rootfs/root
-lxc-attach -n devstack -- bash -c "echo /root/prep-guest.sh | sh"
+cp prep-guest.sh /var/lib/lxc/devstack-node/rootfs/root
+lxc-attach -n devstack-node -- bash -c "echo /root/prep-guest.sh | sh"
 cp localrc /var/lib/lxc/devstack/rootfs/opt/stack/devstack
 cp run-devstack.sh /var/lib/lxc/devstack/rootfs/opt/stack
 lxc-attach -n devstack -- bash -c "echo sudo -H -u stack /opt/stack/run-devstack.sh | sh"
--- prep-guest.sh
+++ prep-guest.sh
@@ -1,7 +1,12 @@
+# Install dependencies
 apt-get update
 apt-get install -qqy bsdmainutils git ca-certificates
-git clone https://github.com/dwalleck/devstack
+
+# Prepare the devstack source files 
+git clone https://github.com/openstack-dev/devstack.git
+sed -i -e 's/sudo sysctl -w net.bridge.bridge-nf-call-${proto}tables=1/echo "Skipping. This breaks when run in a LXC container."/g' devstack/functions
+export HOST_IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 devstack/tools/create-stack-user.sh
 su stack
 cd /opt/stack
-git clone https://github.com/dwalleck/devstack
+git clone https://github.com/openstack-dev/devstack.git
