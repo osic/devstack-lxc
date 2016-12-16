07928a8e1782d4f4d6b4f8b7cb913f701c4eaec4
Castulo J. Martinez <castulo.martinez@intel.com>
2016-12-16 14:37:46 -0800

    Small changes to group by functionality

--- bootstrap-devstack.sh
+++ bootstrap-devstack.sh
@@ -9,21 +9,21 @@
 apt-get update
 apt-get install -qqy git
 
-# Get the devstack repo and add the config file for the controller
-git clone https://github.com/openstack-dev/devstack.git /root/devstack
-controller_ip=$( ip addr show ens3 | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1 )
-sed -ir "s|{ host_ip }|$controller_ip|g" ~/devstack-lxc/local.conf
-cp ~/devstack-lxc/local.conf /root/devstack/local.conf
-
 # Create the "stack" user to use for installing devstack
+controller_ip=$( ip addr show ens3 | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1 )
+git clone https://github.com/openstack-dev/devstack.git /root/devstack
 export HOST_IP=$controller_ip
 /root/devstack/tools/create-stack-user.sh
 
+# Configure devstack for a controller node
+sed -ir "s|{ host_ip }|$controller_ip|g" ~/devstack-lxc/local.conf
+cp ~/devstack-lxc/local.conf /root/devstack/local.conf
+
 # Copy the devstack directory to opt/stack
 cp -r /root/devstack /opt/stack
+sudo chown -R stack:stack  /opt/stack/devstack
 
 # Run the script to install devstack
-sudo chown -R stack:stack  /opt/stack/devstack
 cd /opt/stack/devstack
 sudo -u stack -H sh -c "export HOST_IP=$controller_ip; ./stack.sh"
 
