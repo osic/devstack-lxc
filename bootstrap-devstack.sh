b3570e66b0cb061b85a94d4a9b87dd819062f663
Castulo J. Martinez <castulo.martinez@intel.com>
2016-12-16 11:33:53 -0800

    Add the installation of the controller in the host
    This commit adds the steps for installing desvstack in the host
    VM before creating the node/container and installing devstack on
    it.

--- bootstrap-devstack.sh
+++ bootstrap-devstack.sh
@@ -1,10 +1,36 @@
-# Prepare the devstack source files
+# Prepare the Devstack config files for the controller and the node
+# and copy them to the devstack directory
+
+# ===============
+# Controller Node
+# ===============
+
+# Install dependencies
+apt-get update
+apt-get install -qqy git
+
+# Get the devstack repo and add the config file for the controller
+git clone https://github.com/openstack-dev/devstack.git ~/devstack
+cd ~/devstack
+controller_ip=$( ip addr show ens3 | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1 )
+sed -ir "s|{ host_ip }|$controller_ip|g" ~/devstack-lxc/local.conf
+cp ~/devstack-lxc/local.conf ~/devstack/local.conf
+
+# Run the script to install devstack
+
+
+# ===============
+# Compute Node
+# ===============
+
+# Prepare the devstack source files for the compute node
 cp prep-guest.sh /var/lib/lxc/devstack-node/rootfs/root
 lxc-attach -n devstack-node -- bash -c "echo /root/prep-guest.sh | sh"
-
-# Copy the devstack configuration file to the container
-# TODO: Make changes in the IPs of the config file
-cp local.conf /var/lib/lxc/devstack-node/rootfs/opt/stack/devstack
+node_inet=$( lxc-attach -n devstack-node -- bash -c "echo \"ip addr show eth0 | grep 'inet\b' \" | sh " )
+node_ip=$( echo $node_inet | awk '{print $2}' | cut -d/ -f1 )
+sed -ir "s|{ service_host }|$controller_ip|g" ~/devstack-lxc/compute-node.conf
+sed -ir "s|{ host_ip }|$node_ip|g" ~/devstack-lxc/compute-node.conf
+cp compute-node.conf /var/lib/lxc/devstack-node/rootfs/opt/stack/devstack/local.conf
 
 # Run the script to install devstack
 cp run-devstack.sh /var/lib/lxc/devstack-node/rootfs/opt/stack
--- prep-guest.sh
+++ prep-guest.sh
@@ -3,10 +3,11 @@ apt-get update
 apt-get install -qqy bsdmainutils git ca-certificates
 
 # Prepare the devstack source files 
-git clone https://github.com/openstack-dev/devstack.git
-sed -i -e 's/sudo sysctl -w net.bridge.bridge-nf-call-${proto}tables=1/echo "Skipping. This breaks when run in a LXC container."/g' devstack/functions
+git clone https://github.com/openstack-dev/devstack.git /root/devstack
+sed -i -e 's/sudo sysctl -w net.bridge.bridge-nf-call-${proto}tables=1/echo "Skipping. This breaks when run in a LXC container."/g' /root/devstack/functions
 export HOST_IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-devstack/tools/create-stack-user.sh
+/root/devstack/tools/create-stack-user.sh
+echo "User stack created successfully."
 su stack
-cd /opt/stack
-git clone https://github.com/openstack-dev/devstack.git
+cp -r /root/devstack /opt/stack
+
