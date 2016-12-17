e173d30ac15c64f7fc1b3bccd7909208b26214c1
Castulo J. Martinez <castulo.martinez@intel.com>
2016-12-16 18:53:01 -0800

    Sleeping when creating the container
    Adding a sleep time to allow the the interfaces to go up after
    creating a container.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,6 @@
 # Install dependencies
 apt-get install -qqy lxc lxctl lxc-templates qemu debootstrap
 
-# Change permission on files to make them executable
-chmod +755 bootstrap-devstack.sh
-chmod +755 pre-start.sh
-chmod +755 prep-guest.sh
-chmod +755 run-devstack.sh
-chmod +755 teardown.sh
-
 # Create and start the container
 echo "Creating the lxc container ..."
 lxc-create -t ubuntu -n devstack-node -f devstack-node.conf
@@ -15,3 +8,6 @@ echo "Starting the lxc container  ..."
 lxc-start --name devstack-node && \
 echo "The lxc container devstack-node is active." || \
 echo "The lxc container devstack-node failed to be started."
+
+# Add some sleep time to allow for the interfaces to finish going up
+sleep 5 
