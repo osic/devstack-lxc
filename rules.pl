d5805fd8c6f01403571f3ef903aa5b3bc8583e05
Castulo J. Martinez <castulo.martinez@intel.com>
2017-03-15 14:53:45 -0500

    Rmoving verify from rules
    Change-Id: I5ec11949330e427f6328ddd884ad8459719f74f9

--- rules.pl
+++ rules.pl
@@ -1,13 +1,17 @@
-submit_rule(submit(CR, V)) :-
-    base(CR, V),
+% Rules for merging patches
+% It needs to have at least one +2
+% And the author of the patch can not vote
+
+submit_rule(submit(CR)) :-
+    base(CR),
     CR = label(_, ok(Reviewer)),
     gerrit:commit_author(Author),
     Author \= Reviewer,
     !.
 
-submit_rule(submit(CR, V, N)) :-
-    base(CR, V),
+submit_rule(submit(CR, N)) :-
+    base(CR),
     N = label('Non-Author-Code-Review', need(_)).
 
-base(CR, V) :-
+base(CR) :-
     gerrit:max_with_block(-2, 2, 'Code-Review', CR).
