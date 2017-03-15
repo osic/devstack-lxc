4126d9e544ea4e7cc03538ac626834c0e1886f2c
Castulo J. Martinez <castulo.martinez@intel.com>
2017-03-15 15:06:30 -0500

    more testing
    Change-Id: If94af661a00aec3addab61d78ebccfdea693aa22

--- rules.pl
+++ rules.pl
@@ -2,17 +2,17 @@
 % It needs to have at least one +2
 % And the author of the patch can not vote
 
-submit_rule(submit(CR, V)) :-
-    base(CR, V),
+submit_rule(submit(CR)) :-
+    base(CR),
     CR = label(_, ok(Reviewer)),
     gerrit:commit_author(Author),
     Author \= Reviewer,
     !.
 
-submit_rule(submit(CR, V, N)) :-
-    base(CR, V),
+submit_rule(submit(CR, N)) :-
+    base(CR),
     N = label('Non-Author-Code-Review', need(_)).
 
-base(CR, V) :-
-    gerrit:max_with_block(-2, 2, 'Code-Review', CR),
-    gerrit:max_with_block(-1, 1, 'Verified', V).
+base(CR) :-
+    gerrit:max_with_block(-2, 2, 'Code-Review', CR).
+    
