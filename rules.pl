158ad0de04d5ccb5dedaed34eb14fabb81a7dfcc
Castulo J. Martinez <castulo.martinez@intel.com>
2017-03-21 16:38:46 -0500

    Testing Gerrit rules
    Change-Id: I825da53657a43ea1e33810e3a2ab158a9c44701c

--- rules.pl
+++ rules.pl
@@ -23,7 +23,7 @@
 
 submit_rule(submit(CR)) :-
     gerrit:max_with_block(-2, 2, 'Code-Review', CR),
-    gerrit:commit_author(_, Author, -),
+    gerrit:commit_author(Author),
     'Castulo J. Martinez' = 'Castulo J. Martinez',
     !.
 
