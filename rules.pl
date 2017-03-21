b1c38c6915c2a0df6192e83e9a73f0fdb082f9d3
Castulo J. Martinez <castulo.martinez@intel.com>
2017-03-21 12:26:58 -0500

    Testing rules 1
    Change-Id: I8c6db2ca57573cc95827516d3fc90c57672021bd

--- rules.pl
+++ rules.pl
@@ -1,19 +1,26 @@
 % Rules for merging patches
 % It needs to have at least one +2
 % And the author of the patch can not vote
+%
+%submit_rule(submit(CR)) :-
+%    base(CR),
+%    CR = label(_, ok(Reviewer)),
+%    gerrit:commit_author(Author),
+%    Author \= Reviewer,
+%    !.
+%
+%submit_rule(submit(CR, N)) :-
+%    base(CR),
+%    N = label('Non-Author-Code-Review', need(_)).
+%
+%base(CR) :-
+%    gerrit:max_with_block(-2, 2, 'Code-Review', CR).
 
-submit_rule(submit(CR)) :-
-    base(CR),
-    CR = label(_, ok(Reviewer)),
-    gerrit:commit_author(Author),
-    \+ (Author = Reviewer),
-    Author \= Reviewer,
-    !.
+% These are some sample Facts like the ones Gerrit will provide for each patch 
+% commit_author(user(1000000), 'Castulo J. Martinez', 'castulo.martinez@intel.com').
+% commit_committer(user(1000000), 'Castulo J. Martinez', 'castulo.martinez@intel.com').
+% commit_message('Add plugin support to Gerrit').
 
-submit_rule(submit(CR, N)) :-
-    base(CR),
-    N = label('Non-Author-Code-Review', need(_)).
+submit_rule(X) :-
+    X = submit(label('Code-Review', ok(user(ID)))).
 
-base(CR) :-
-    gerrit:max_with_block(-2, 2, 'Code-Review', CR).
-    
