7e763d9c3b050c666246e5d6d6999d0bb231920b
Castulo J. Martinez <castulo.martinez@intel.com>
2017-03-21 15:08:32 -0500

    Testing Gerrit rules
    Change-Id: I09376d6db50bf28f6539e47667a932896670bcf5

--- rules.pl
+++ rules.pl
@@ -23,8 +23,10 @@
 
 submit_rule(submit(CR)) :-
     %CR = label(_, ok(Reviewer)).
-    CR = label(_, ok(user(1))).
-    %gerrit:max_with_block(-2, 2, 'Code-Review', CR),
-    %gerrit:commit_author(Author),
-    
+    %CR = label(_, ok(user(1))).
+    gerrit:max_with_block(-2, 2, 'Code-Review', CR),
+    gerrit:current_user(Reviewer),
+    gerrit:commit_author(Author),
+    Author \= Reviewer.
+    %gerrit:commit_author(Author), 
     %CR = label('Code-Review', ok(user(1))).
