5b89b0fe6766dfc47e397780d7a60d760db97179
Castulo J. Martinez <castulo.martinez@intel.com>
2017-03-21 15:40:51 -0500

    Testing Gerrit rules
    Change-Id: I5797f5cdad5df086675785bab02752de91a99673

--- rules.pl
+++ rules.pl
@@ -1,20 +1,6 @@
 % Rules for merging patches
 % It needs to have at least one +2
 % And the author of the patch can not vote
-%
-%submit_rule(submit(CR)) :-
-%    base(CR),
-%    CR = label(_, ok(Reviewer)),
-%    gerrit:commit_author(Author),
-%    Author \= Reviewer,
-%    !.
-%
-%submit_rule(submit(CR, N)) :-
-%    base(CR),
-%    N = label('Non-Author-Code-Review', need(_)).
-%
-%base(CR) :-
-%    gerrit:max_with_block(-2, 2, 'Code-Review', CR).
 
 % These are some sample Facts like the ones Gerrit will provide for each patch 
 % commit_author(user(1000000), 'Castulo J. Martinez', 'castulo.martinez@intel.com').
@@ -23,8 +9,8 @@
 
 submit_rule(submit(CR)) :-
     base(CR),
-    CR = label(_, ok(Reviewer)),
-    gerrit:commit_author(Author),
+    CR = label(_, ok(user(Reviewer))),
+    gerrit:commit_author(user(Author)),
     Author \= Reviewer,
     !.
 
