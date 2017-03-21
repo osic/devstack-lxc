2874dfae89f25d7d8a45aec024afd6cd1c87bf0d
Castulo J. Martinez <castulo.martinez@intel.com>
2017-03-21 16:01:16 -0500

    Testing Gerrit rules
    Change-Id: I74e2c55f009cb4b25038301bfd2bdba4a3a31e9b

--- rules.pl
+++ rules.pl
@@ -21,8 +21,8 @@
 %base(CR) :-
 %    gerrit:max_with_block(-2, 2, 'Code-Review', CR).
 
-submit_rule(submit(CR)) :-
-    gerrit:max_with_block(-2, 2, 'Code-Review', CR).
-
 submit_rule(submit(NAR)) :-
     NAR = label('Non-Author-Code-Review', need(_)).
+
+submit_rule(submit(CR)) :-
+    gerrit:max_with_block(-2, 2, 'Code-Review', CR).
